90a488d91d9319b0227cceafb92f8ca8f9e8c84d
kochenov <kochenof@yandex.ru>
2024-07-23 12:56:05 +0300

    старт

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="q-pa-md">
+    <work-schedule />
+  </q-page>
+</template>
+
+<script>
+import WorkSchedule from 'src/components/WorkSchedule.vue';
+
+export default {
+  components: {
+    WorkSchedule,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
